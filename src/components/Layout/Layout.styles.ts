be65a7562af991d7594191100f5d48a9d50eea67
hanna <annisokay97@gmail.com>
2020-04-29 11:44:17 +0300

    refactor(Layout): refactor styles

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -6,23 +6,29 @@ import { Mixin } from 'ustudio-ui/theme';
 const Layout = styled.div`
   height: 100%;
   min-height: 100vh;
+
   display: flex;
   flex-direction: column;
 `;
 
 const Header = styled.header`
   height: 64px;
+
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   padding: var(--i-regular) var(--i-large);
+
   box-shadow: var(--s-light);
 `;
 
 const LogoLink = styled(Link)`
   max-height: 3rem;
+
   display: flex;
   align-items: center;
+
   &:after {
     content: none;
   }
@@ -30,13 +36,13 @@ const LogoLink = styled(Link)`
 
 const LogoImage = styled.img`
   width: 2rem;
+
   margin-right: var(--i-medium);
 `;
 
 const LogoText = styled.span`
-  line-height: 1;
-  font-size: 24px;
-  font-weight: 700;
+  ${Mixin.Font.h3()};
+
   color: var(--c-darkest);
   user-select: none;
 `;
@@ -46,10 +52,12 @@ const Main = styled.main`
 `;
 
 const Footer = styled.footer`
-  padding: var(--i-small) 0;
-  background-color: var(--c-light);
   ${Mixin.Font.bodySmall()};
+
+  padding: var(--i-small) 0;
   text-align: center;
+
+  background-color: var(--c-light);
 `;
 
 export default {
