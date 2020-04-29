d122685c46075a492c9e01222d1ab0ba40dcf6b4
hanna <annisokay97@gmail.com>
2020-04-29 11:26:06 +0300

    refactor: rename theme.json to config.json, add docsFolder field

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { HashRouter } from 'react-router-dom';
 
 import { ThemeProvider } from 'ustudio-ui/theme';
 
-import { palette } from './assets/theme.json';
+import { palette } from './assets/config.json';
 import { Layout } from './components/Layout';
 
 const App = () => {
