17d0fda6edac0ad1ff6e31b0db4a93b03a6a0d34
hanna <annisokay97@gmail.com>
2020-04-29 11:02:46 +0300

    refactor(Layout): remove fragment

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -6,25 +6,23 @@ import Styled from './Layout.styles';
 
 export const Layout: FC = ({ children }) => {
   return (
-    <>
-      <Styled.Layout>
-        <Styled.Header>
-          <Styled.LogoLink to="/">
-            <Styled.LogoImage src={logo} alt="uDocumentation Logo" />
-            <Styled.LogoText>uDocumentation</Styled.LogoText>
-          </Styled.LogoLink>
-        </Styled.Header>
+    <Styled.Layout>
+      <Styled.Header>
+        <Styled.LogoLink to="/">
+          <Styled.LogoImage src={logo} alt="uDocumentation Logo" />
+          <Styled.LogoText>uDocumentation</Styled.LogoText>
+        </Styled.LogoLink>
+      </Styled.Header>
 
-        <Styled.Main>{children}</Styled.Main>
+      <Styled.Main>{children}</Styled.Main>
 
-        <Styled.Footer>
-          © 2020{' '}
-          <a href="https://ustudio.company" target="_blank" rel="noreferrer noopener">
-            uStudio LLC
-          </a>{' '}
-          ❤️
-        </Styled.Footer>
-      </Styled.Layout>
-    </>
+      <Styled.Footer>
+        © 2020{' '}
+        <a href="https://ustudio.company" target="_blank" rel="noreferrer noopener">
+          uStudio LLC
+        </a>{' '}
+        ❤️
+      </Styled.Footer>
+    </Styled.Layout>
   );
 };
