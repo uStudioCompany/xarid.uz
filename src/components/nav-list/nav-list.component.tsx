0a6d6f1f6611824aa5353cbb30d84ffcbd0f3a3e
Dmitriy Volkov <mr.rotberry@gmail.com>
2020-05-04 18:10:18 +0300

    feat: Add filter on documents format in aside

--- src/components/nav-list/nav-list.component.tsx
+++ src/components/nav-list/nav-list.component.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import Placeholder from 'ustudio-ui/components/Placeholder';
+import Text from 'ustudio-ui/components/Text';
 
 import { NavItem } from '../nav-item';
 
@@ -19,19 +20,27 @@ export const NavList = ({ tree, prevPath, isLoading }: { tree: Node[]; prevPath:
     return <Placeholder variant="text" appearance={{ width: `${getRandomPlaceholderWidth()}%`, height: 'body' }} />;
   }
 
+  const filteredList = tree?.filter(({ type, name }) => type === 'tree' || (type === 'blob' && /.md$/.test(name)));
+
   return (
     <>
-      {tree?.map((node) => {
-        const parsedDocName = node.name.replace('.md', '');
+      {filteredList.length ? (
+        filteredList.map((node) => {
+          const parsedDocName = node.name.replace('.md', '');
 
-        return node.type === 'tree' ? (
-          <NavItem key={node.name} node={node} prevPath={prevPath} />
-        ) : (
-          <Styled.Button type="button" onClick={setDrawerState} key={node.name}>
-            <Link to={`/${prevPath}/${parsedDocName}`}>{parsedDocName}</Link>
-          </Styled.Button>
-        );
-      })}
+          return node.type === 'tree' ? (
+            <NavItem key={node.name} node={node} prevPath={prevPath} />
+          ) : (
+            <Styled.Button type="button" onClick={setDrawerState} key={node.name}>
+              <Link to={`/${prevPath}/${parsedDocName}`}>{parsedDocName}</Link>
+            </Styled.Button>
+          );
+        })
+      ) : (
+        <Text variant="small" color="#333">
+          No resources
+        </Text>
+      )}
     </>
   );
 };
