29124fb5b417b75f102e2e29197ad8ec0b0783db
Hanna <23137619+annisokay97@users.noreply.github.com>
2020-08-06 11:07:47 +0300

    Fix: Fix error, when use nesting in heading (#25)

--- src/shared/components/markdown/renderers.module.tsx
+++ src/shared/components/markdown/renderers.module.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Renderers } from 'react-markdown';
+import { Renderers, MarkdownAbstractSyntaxTree } from 'react-markdown';
+import Code from 'react-syntax-highlighter/dist/cjs/prism';
 
 import Styled from './markdown.styles';
 
@@ -9,23 +10,50 @@ import { JsonSchema } from './json-schema';
 export const renderers: Renderers = {
   thematicBreak: Styled.Divider,
   root: Styled.Root,
-  heading: ({ children, level }) => (
-    <Styled.Heading
-      as={`h${level}` as 'h1'}
-      id={children[0]?.props?.children
-        .split(' ')
-        .reduce((id: string, word: string) => `${id ? `${id}-` : ''}${word}`, ``)
-        .toLowerCase()}
-    >
-      {children}
-    </Styled.Heading>
-  ),
+  heading: ({ children, level }) => {
+    const getRecursiveChildren = (children: MarkdownAbstractSyntaxTree): string => {
+      if (Array.isArray(children)) {
+        return getRecursiveChildren(children[0]?.props?.children);
+      }
+
+      return children as unknown as string;
+    };
+
+    return (
+      <Styled.Heading
+        id={getRecursiveChildren(children)
+          ?.split(' ')
+          ?.reduce((id: string, word: string) => `${id ? `${id}-` : ''}${word}`, ``)
+          ?.toLowerCase()}
+        as={`h${level}` as 'h1'}
+      >
+        {children}
+      </Styled.Heading>
+    );
+  },
   blockquote: Styled.Quote,
   paragraph: Styled.Paragraph,
   list: ({ depth, children, ordered }) => (
     <Styled.List depth={depth} ordered={ordered}>
       {children}
     </Styled.List>
+  ),
+  code: ({ value, language }: { value: string; language: string }) => (
+    <Code
+      language={language}
+      customStyle={{
+        padding: 'var(--i-regular) 0',
+        marginBottom: '0',
+        marginTop: '0',
+      }}
+      codeTagProps={{
+        style: {
+          whiteSpace: 'pre-wrap',
+        },
+      }}
+    >
+      {value}
+    </Code>
   ),
   link: ({ href, children, title }: { href: string; children: string; title?: string }) => {
     if (/^.+\.csv/.test(href)) {
